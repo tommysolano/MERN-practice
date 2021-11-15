2758740d6c008e9c34c7ede30ac9b2bbd6ba8879
tommy <tommysolano18@hotmail.com>
2021-11-15 03:57:23 -0500

    Building out Routes and Controllers

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,15 @@
 require('dotenv').config()
 const express = require('express');
 const connectDB = require('./config/db')
+const productRoutes = require("./routes/productsRoutes.js")
 
 connectDB()
 
 const app = express();
+
+app.use(express.json())
+
+app.use("/api/products", productRoutes)
 
 const PORT = process.env.PORT || 5000
 
